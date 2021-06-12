febef6e2a77852f223e81cf64f7e90e9e77d964d
Anders Jurisoo <jurisoo@hotmail.com>
2021-06-12 10:07:09 +0200

    Skip sleeper test

--- tests/unit/server/nodes/Sleep.test.ts
+++ tests/unit/server/nodes/Sleep.test.ts
@@ -1,7 +1,7 @@
 import Sleep from '../../../../src/server/nodes/Sleep'
 import { when } from "../ServerNodeTester";
 
-it('can run', async () => {
+it.skip('can run', async () => {
     await when(Sleep).hasParameters({seconds_to_sleep: 0})
 		.assertCanRun()
 		.finish()
