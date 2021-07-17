5501aed84a91b18adde7eb6a24a4eda27e08e8c5
Anders Jurisoo <jurisoo@hotmail.com>
2021-07-17 12:51:49 +0200

    This repo has moved

--- src/core/clients/ClientFactory.ts
+++
@@ -1,9 +0,0 @@
-import APIClient from './APIClient';
-import LocalClient from './LocalClient';
-
-const clients = {
-    APIClient,
-    LocalClient
-}
-
-export default (name: string) => new clients[name]
--- src/server/ServerPort.ts
+++
@@ -1,7 +0,0 @@
-// import Feature from "../core/Feature";
-
-// export type ServerPort = {
-// 	name: string,
-// 	features: any[],
-// 	links: any[]
-// }
